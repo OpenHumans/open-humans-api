0cc51e9219c481f608d4e0b08389e6ccbe19c816
DartingMelody <tkhaniitr@gmail.com>
2018-03-28 21:54:25 +0530

    Docs for projects.py (#60)

--- ohapi/projects.py
+++ ohapi/projects.py
@@ -21,6 +21,11 @@ class OHProject:
 
     @staticmethod
     def _get_member_file_data(member_data):
+        """
+        Helper function to get file data of member of a project.
+
+        :param member_data: This field is data related to member in a project.
+        """
         file_data = {}
         for datafile in member_data['data']:
             basename = datafile['basename']
@@ -31,6 +36,9 @@ class OHProject:
         return file_data
 
     def update_data(self):
+        """
+        Returns data for all users including shared data files.
+        """
         url = ('https://www.openhumans.org/api/direct-sharing/project/'
                'members/?access_token={}'.format(self.master_access_token))
         results = get_all_results(url)
@@ -43,6 +51,12 @@ class OHProject:
                                      max_size=MAX_SIZE_DEFAULT):
         """
         Download files to sync a local dir to match OH member project data.
+
+        :param member_data: This field is data related to member in a project.
+        :param target_member_dir: This field is the target directory where data
+            will be downloaded.
+        :param max_size: This field is the maximum file size. It's default
+            value is 128m.
         """
         logging.debug('Download member project data...')
         sources_shared = member_data['sources_shared']
@@ -67,6 +81,13 @@ class OHProject:
         Files are downloaded to match their "basename" on Open Humans.
         If there are multiple files with the same name, the most recent is
         downloaded.
+
+        :param member_data: This field is data related to member in a project.
+        :param target_member_dir: This field is the target directory where data
+            will be downloaded.
+        :param source: This field is the source from which to download data.
+        :param max_size: This field is the maximum file size. It's default
+            value is 128m.
         """
         logging.debug('Download member shared data...')
         sources_shared = member_data['sources_shared']
@@ -99,6 +120,21 @@ class OHProject:
     def download_all(self, target_dir, source=None, project_data=False,
                      memberlist=None, excludelist=None,
                      max_size=MAX_SIZE_DEFAULT):
+        """
+        Download data for all users including shared data files.
+
+        :param target_dir: This field is the target directory to download data.
+        :param source: This field is the data source. It's default value is
+            None.
+        :param project_data: This field is data related to particular project.
+            It's default value is False.
+        :param memberlist: This field is list of members whose data will be
+            downloaded. It's default value is None.
+        :param excludelist: This field is list of members whose data will be
+            skipped. It's default value is None.
+        :param max_size: This field is the maximum file size. It's default
+            value is 128m.
+        """
         members = self.project_data.keys()
         for member in members:
             if not (memberlist is None) and member not in memberlist:
@@ -137,6 +173,16 @@ class OHProject:
 
         If the 'mode' parameter is 'sync': files on Open Humans that are not
         in the local directory will be deleted.
+
+        :param member_data: This field is data related to member in a project.
+        :param target_member_dir: This field is the target directory from where
+            data will be uploaded.
+        :param metadata: This field is metadata for files to be uploaded.
+        :param access_token: This field is user specific access token.
+        :param mode: This field takes three value default, sync, safe. It's
+            default value is 'default'.
+        :param max_size: This field is the maximum file size. It's default
+            value is 128m.
         """
         if not validate_metadata(target_member_dir, metadata):
             raise ValueError('Metadata should match directory contents!')
